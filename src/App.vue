6778c6dfd7fa4527cc472d00b3abbf47517604e4
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2024-06-04 01:53:52 +0300

    2

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@
         frameborder="0"
         allowfullscreen
       ></iframe>
+      <p>Поверни телефон</p>
+    </div>
+    <div class="fireworks" v-if="isFirework">
+      <div class="firework" id="firework-left"></div>
+      <div class="firework" id="firework-right"></div>
     </div>
     <div class="balloons" v-if="!started">
       <div v-for="n in balloons" :key="n" class="balloon"></div>
@@ -21,11 +26,20 @@
 export default {
   data() {
     return {
+      isFirework: false,
       started: false,
       balloons: 5
     };
   },
   mounted() {
+    window.addEventListener('load', () => {
+      this.isFirework = true
+
+      setTimeout(() => {
+        this.isFirework = false
+      }, 3000)
+    })
+
     this.getNumber()
     window.addEventListener('resize', this.getNumber)
   },
@@ -45,6 +59,9 @@ export default {
 
 :root {
   --w: min(100vw, 1450px);
+  --dim-green: #369b72;
+  --light-green: #42b983;
+  --blue-balloon: rgba(0, 0, 255, 0.6);
 }
 
 html {
@@ -54,7 +71,9 @@ body {
   margin: 0;
   text-align: center;
   background: linear-gradient(orange, rgb(126, 8, 126));
+  overflow: hidden;
 }
+
 .greeting {
   margin-top: calc(50vh - ((2.5em * 1.2 * 2 + 20px + 1.5em + 20px) / 2));
 }
@@ -68,26 +87,51 @@ body {
 .greeting button {
   font-size: 1.5em;
   padding: 10px 20px;
-  background-color: #42b983;
+  background-color: var(--light-green);
   color: white;
   border: none;
   border-radius: 5px;
   cursor: pointer;
 }
 .greeting button:hover {
-  background-color: #369b72;
+  background-color: var(--dim-green);
 }
 
 .video-container {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   height: 100vh;
 }
 
 iframe {
   width: var(--w);
   height: calc(var(--w) * 0.5625);
+}
+
+p {
+  margin: 0;
+  font-size: 27px;
+  line-height: 2;
+  animation: 1s flickering linear infinite;
+}
+
+.firework {
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  border-radius: 50%;
+  position: absolute;
+  top: 0;
+}
+#firework-left {
+  left: 0;
+  animation: firework-left-show 3s;
+}
+#firework-right {
+  right: 0;
+  animation: firework-right-show 3s;
 }
 
 .balloons {
@@ -98,21 +142,19 @@ iframe {
   justify-content: space-around;
   pointer-events: none;
 }
-
 .balloon {
   width: 50px;
   height: 70px;
-  background-color: rgba(0, 0, 255, 0.6);
+  background-color: var(--blue-balloon);
   border-radius: 50%;
   position: relative;
   animation: floatUp 5s linear infinite;
 }
-
 .balloon::before {
   content: "";
   width: 10px;
   height: 20px;
-  background-color: rgba(0, 0, 255, 0.6);
+  background-color: var(--blue-balloon);
   position: absolute;
   bottom: -10px;
   left: 50%;
@@ -120,8 +162,81 @@ iframe {
   border-radius: 10px 10px 0 0;
 }
 
+@keyframes firework-left-show {
+  0% {
+    transform: translate(0%, 0%) scale(0);
+    opacity: 0;
+  }
+  33% {
+    transform: translate(120%, 120%) scale(1);
+    opacity: 1;
+  }
+  34% {
+    transform: translate(120%, 120%) scale(0);
+    opacity: 0;
+  }
+  67% {
+    transform: translate(240%, 240%) scale(1);
+    opacity: 1;
+  }
+  68% {
+    transform: translate(240%, 240%) scale(0);
+    opacity: 0;
+  }
+  90% {
+    transform: translate(350%, 350%) scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(350%, 350%) scale(1);
+    opacity: 0;
+  }
+}
+
+@keyframes firework-right-show {
+  0% {
+    transform: translate(0%, 0%) scale(0);
+    opacity: 0;
+  }
+  33% {
+    transform: translate(-120%, 120%) scale(1);
+    opacity: 1;
+  }
+  34% {
+    transform: translate(-120%, 120%) scale(0);
+    opacity: 0;
+  }
+  67% {
+    transform: translate(-240%, 240%) scale(1);
+    opacity: 1;
+  }
+  68% {
+    transform: translate(-240%, 240%) scale(0);
+    opacity: 0;
+  }
+  90% {
+    transform: translate(-350%, 350%) scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(-350%, 350%) scale(1);
+    opacity: 0;
+  }
+}
+
 @keyframes floatUp {
-  0% {transform: translateY(0)}
-  100% {transform: translateY(-100vh)}
+  0% {transform: translateY(0);}
+  100% {transform: translateY(-100vh);}
+}
+
+@keyframes flickering {
+  0% {color: var(--dim-green);}
+  100% {color: var(--light-green);}
+}
+
+@media (orientation: landscape), (min-width: 601px) {
+  p {
+    display: none;
+  }
 }
 </style>
